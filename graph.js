478d217cb6e898dbd94cd60c56351b0d32c3edc0
Alexander <liljaale@arcada.fi>
2018-10-10 19:52:44 +0300

    Initial commit

---
+++ graph.js
@@ -0,0 +1,38 @@
+var nodes = []
+var links = []
+
+d3.json("facebookFriends.json").then((data) => {
+
+var colour = "lightgrey";
+
+var canvas = d3.select("body")
+   .append("div")
+   .classed("svg-container", true) //container class to make it responsive
+   .append("svg")
+   //responsive SVG needs these 2 attributes and no width and height attr
+   .attr("preserveAspectRatio", "xMinYMin meet")
+   .attr("viewBox", "0 0 400 400")
+   //class to make it responsive
+   .classed("svg-content-responsive", true)
+   .style("background", colour);
+
+
+    //loop through all entries
+    for (let key in data['facebookFriends']) {
+        let id = data['facebookFriends'][key]['FacebookID']
+        let name = data['facebookFriends'][key]['name']
+        let friends = data['facebookFriends'][key]['friends']
+
+        //create the nodes
+        nodes.push({'id': id, 'name': name})
+        
+        //get the friends for each entry and create the links
+        for(let i in friends){
+            let friend = friends[i]['FriendId']
+            links.push({'source': id, 'target': friend})
+            
+        }
+    }
+    console.log(nodes);
+    console.log(links);
+});
