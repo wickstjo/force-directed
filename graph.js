942bf13b506116562b9bdf9a8d84b02efb4ff440
Alexander <liljaale@arcada.fi>
2018-10-10 20:09:14 +0300

    New update

--- graph.js
+++ graph.js
@@ -8,21 +8,20 @@ var nodes = [];
 var links = [];
 
 var canvas = d3.select("body")
-   .append("div")
-   .classed("svg-container", true) //container class to make it responsive
-   .append("svg")
-   //responsive SVG needs these 2 attributes and no width and height attr
-   .attr("preserveAspectRatio", "xMinYMin meet")
-   .attr("viewBox", "0 0 400 400")
-   //class to make it responsive
-   .classed("svg-content-responsive", true)
-   .style("background", colour);
-
+    .append("div")
+    .classed("svg-container", true) //container class to make it responsive
+    .append("svg")
+    //responsive SVG needs these 2 attributes and no width and height attr
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 400 400")
+    //class to make it responsive
+    .classed("svg-content-responsive", true)
+    .style("background", colour);    
 
     //loop through all entries
     for (let key in data['facebookFriends']) {
-        let id = data['facebookFriends'][key]['FacebookID']
         let name = data['facebookFriends'][key]['name']
+        let id = data['facebookFriends'][key]['FacebookID']
         let friends = data['facebookFriends'][key]['friends']
 
         //create the nodes
